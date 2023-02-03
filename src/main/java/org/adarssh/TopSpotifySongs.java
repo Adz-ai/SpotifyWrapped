8daa9f7f421cf751ab5cf166960dfc0d809b441c
Adz-ai <adarssh@live.co.uk>
2023-02-03 00:08:36 +0000

    Created TopSpotifySongs

---
+++ src/main/java/org/adarssh/TopSpotifySongs.java
@@ -0,0 +1,49 @@
+package org.adarssh;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class TopSpotifySongs {
+
+    private static final String API_URL = "https://api.spotify.com/v1/me/top/tracks?limit=5";
+
+    public void outputTopSpotifySongs(String ACCESS_TOKEN) {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization", "Bearer " + ACCESS_TOKEN);
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            int status = connection.getResponseCode();
+            if (status == 200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+                reader.close();
+                JSONObject response = new JSONObject(builder.toString());
+                JSONArray items = response.getJSONArray("items");
+                System.out.println("My top Spotify songs of the month are:");
+                for (int i = 0; i < items.length(); i++) {
+                    JSONObject item = items.getJSONObject(i);
+                    String name = item.getString("name");
+                    System.out.println(name);
+                }
+            } else {
+                System.out.println("Failed to retrieve top Spotify songs: " + connection.getResponseMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
